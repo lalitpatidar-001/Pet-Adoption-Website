4688fadb74fd8f7789f38b5ce3c346d8c8f3c47f
Lalit <lalitpatidar388@gmail.com>
2024-05-07 12:25:43 +0530

    added vercel backend path url

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/api', // Base URL for all requests
+  baseURL: 'https://api-pet-adoption.vercel.app/api', // Base URL for all requests
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export const STATIC_PATH = "http://localhost:4000/"
+export const STATIC_PATH = "https://api-pet-adoption.vercel.app/"
 
 export default axiosInstance;
