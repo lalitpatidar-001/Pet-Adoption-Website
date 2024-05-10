1333bcea15fa41672031500b0586585c9b1a6b7c
Lalit <lalitpatidar388@gmail.com>
2024-05-11 00:07:04 +0530

    fixed typos in axios.js

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 // https://api-pet-adoption.vercel.app/api
-https://api-pet-adoption-website.vercel.app/
 
 const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE==="Development" ? "http://localhost:4000":"https://api-pet-adoption-website.vercel.app"
 
