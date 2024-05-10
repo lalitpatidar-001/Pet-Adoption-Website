1468ae310abe525bf2c50e83ba30c7986bf88952
Lalit <lalitpatidar388@gmail.com>
2024-05-11 00:04:24 +0530

    changed new url of backenc to forntend

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 // https://api-pet-adoption.vercel.app/api
+https://api-pet-adoption-website.vercel.app/
 
-const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE==="Development" ? "http://localhost:4000":"https://api-pet-adoption.vercel.app"
+const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE==="Development" ? "http://localhost:4000":"https://api-pet-adoption-website.vercel.app"
 
 const axiosInstance = axios.create({
   baseURL: `${urlPath}/api`, // Base URL for all requests
