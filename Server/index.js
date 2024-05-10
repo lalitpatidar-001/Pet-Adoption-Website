1ba0c1f8cb9d1de1424a0c656b0018673054a88d
Lalit <lalitpatidar388@gmail.com>
2024-05-10 23:27:33 +0530

    updated previous cors

--- Server/index.js
+++ Server/index.js
@@ -18,13 +18,13 @@ const messageRouter = require('./routers/message');
 
 app.use((req, res, next) => {
   const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
+  // const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  // if (isWhitelisted) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
-  }
+  // }
   // Pass to next layer of middleware
   if (req.method === 'OPTIONS') res.sendStatus(200);
   else next();
