c7c2d8b40ae2d3708d31f1897d83c32ede7dae8b
Lalit <lalitpatidar388@gmail.com>
2024-06-18 20:25:24 +0530

    changed render backend url

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // https://api-pet-adoption.vercel.app/api
 
-export const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE==="Development" ? "http://localhost:4000":"https://api-pet-adoption-website.vercel.app"
+export const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE==="Development" ? "http://localhost:4000":"https://api-pet-adoption-website.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: `${urlPath}/api`, // Base URL for all requests
