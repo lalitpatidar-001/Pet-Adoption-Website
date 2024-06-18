4dbe3d71ca0f5b8d8a1294674fb167d7f5b47d00
Lalit <lalitpatidar388@gmail.com>
2024-06-18 20:30:12 +0530

    added api

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 
 // https://api-pet-adoption.vercel.app/api
 
-export const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE==="Development" ? "http://localhost:4000":"https://api-pet-adoption-website.onrender.com"
+export const urlPath = import.meta.env.VITE_REACT_APP_ENV_MODE === "Development" ? "http://localhost:4000" : "https://api-pet-adoption-website.onrender.com"
 
 const axiosInstance = axios.create({
-  baseURL: `${urlPath}/api`, // Base URL for all requests
+  baseURL: urlPath + "/api", // Base URL for all requests
   headers: {
     'Content-Type': 'application/json',
   },
   credentials: true
 });
 
-export const STATIC_PATH = urlPath+"/"
+export const STATIC_PATH = urlPath + "/"
 
 export default axiosInstance;
